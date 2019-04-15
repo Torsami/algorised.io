910c7d9851a2c12157fc735297e3efbd65f51cdb
torsami <torsami77@gmail.com>
2019-04-15 11:50:45 +0100

    db-credentials-files-updated

--- cert.php
+++ cert.php
@@ -1,7 +1,7 @@
 <?php
 $institution = 'etsk' ;
 $website = 'www.etsk.sch.ng' ;
-$db = new mysqli('localhost', 'root', 'eduportal101', 'db');
+$db = new mysqli('remotemysql.com', 'O5l0hXf4FI', 'AUYwGA2uNj', 'O5l0hXf4FI');
 
 if($db->connect_errno){
 	die('Sorry unable to connect to database at the moment, Please try again later.');
